57824143e4662004606617d7885a9c4cb7d0cdf5
Your jazahraoui <zahraoui.jawad8@gmail.com>
2025-06-27 10:40:39 +0100

    push

--- functions/capitalize (1).go
+++ functions/capitalize (1).go
@@ -1,13 +1,20 @@
 package goreloaded
 
 import (
-    "strings"
+    "unicode"
 )
 
-  func Capitalize(s string) string {
-	if len(s) == 0 {
-		return ""
-	}
-	return strings.ToUpper(s[:1]) + strings.ToLower(s[1:])
+func Capitalize(s string) string {
+    slice := []rune(s)
+    flag := false
+    
+    for i := 0; i < len(slice); i++ {
+        if unicode.IsLetter(slice[i]) && !flag {
+            slice[i] = unicode.ToUpper(slice[i])
+            flag = true
+        } else if flag {
+            slice[i] = unicode.ToLower(slice[i])
+        }
+    }
+    return string(slice)
 }
-
